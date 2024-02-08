23292812c6e1c173ab5ea20b1492b08e0e2497ee
Harsheni Siddharthan <hsiddharthan@v320-014.ls6.tacc.utexas.edu>
2024-02-07 22:05:12 -0600

    nice

---
+++ homework3/build/CMakeFiles/scangather.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/scangather.dir/scangather.c.o"
+  "CMakeFiles/scangather.dir/scangather.c.o.d"
+  "scangather"
+  "scangather.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/scangather.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ homework3/build/CMakeFiles/scangather.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for scangather.
+# This may be replaced when dependencies are built.
